03a6e92c513bdd2119cf109878d88e2c3ab35195
fallGamlet <fallgamlet@yandex.ru>
2021-10-03 18:23:50 +0300

    make server and client example for demonstrate TCP

--- tcp-nodejs/client.js
+++ tcp-nodejs/client.js
@@ -1,32 +1,40 @@
 const Net = require('net');
 
+/**
+ * Example client class for demonstrate how to connect, send and recieve data as TCP client 
+ * connected to the server
+ * recieving server messages
+ * sending several messages to the server
+ */
 class Client {
     constructor(id) {
         this.id = id;
-        this.client = new Net.Socket();
-        this.client.on('data', (chunk) => {
-            console.log(`Client#${this.id}: ${chunk.toString()}\n`);
+        this.socket = new Net.Socket();
+        this.socket.on('data', (chunk) => {
+            console.log(`Client#${this.id}: ${chunk}\n`);
         });
-        this.client.on('end', function() {
+        this.socket.on('end', function() {
             console.log('Requested an end to the TCP connection');
         });
     }
 
-    connect() {
-        const port = 8888;
-        const host = 'localhost';
-        this.client.connect({ port: port, host: host }, () => {
-            console.log(`Client#${this.id} connected`);
-            this.client.write(`Hello, server. I am client #${this.id}`);
-        });
-    }
-
-    disconnect() {
-        this.client.end();
+    connect(host, port) {
+        this.socket.connect(
+            { port: port, host: host }, 
+            () => {
+                console.log(`Client#${this.id} connected`);
+                this.socket.write(`Hello, server. I am client #${this.id}`);
+                this.socket.write(`client #${this.id} message 1`);
+                this.socket.write(`client #${this.id} message 2`);
+                this.socket.write(`client #${this.id} message 3`);
+                this.socket.end()
+            }
+        );
     }
 }
 
-for (let i = 0; i < 10; i++) {
-    const client = new Client(i);
-    client.connect();
+const port = 8080;
+const host = 'localhost';
+for (let i = 0; i < 3; i++) {
+    new Client(i).connect(host, port);
 }
--- tcp-nodejs/server.js
+++ tcp-nodejs/server.js
@@ -1,14 +1,15 @@
+/// Server listen of a client connect
+/// send a message 'Hello, Client.' when client connected
+/// server recieve data from connected client
+
 const Net = require('net');
 const port = 8080;
 
-const server = new Net.Server();
-server.listen(port, function() {
-    console.log(`Server listening for connection requests on socket localhost:${port}`);
-});
-
-server.on('connection', function(socket) {
+const clientHandler = function(socket) {
     console.log('A new connection has been established.');
+    
     socket.write('Hello, client.');
+    
     socket.on('data', function(chunk) {
         console.log(`Data received from client: ${chunk.toString()}`);
     });
@@ -20,4 +21,10 @@ server.on('connection', function(socket) {
     socket.on('error', function(err) {
         console.log(`Error: ${err}`);
     });
+};
+
+const server = new Net.Server();
+server.on('connection', clientHandler);
+server.listen(port, function() {
+    console.log(`Server listening for connection requests on socket localhost:${port}`);
 });
